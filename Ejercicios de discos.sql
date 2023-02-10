8ea6dfdcbd7d9333489f85f69396ffe16212e196
Alejandro <alejandroasp71@gmail.com>
2023-02-10 13:49:15 +0000

    terminando ejercicios de inner join

--- Ejercicios de discos.sql
+++ Ejercicios de discos.sql
@@ -1,23 +1,61 @@
 --1.- Cuáles son los dos clientes con más puntuaciones efectuadas (sacándolos todos).
-select 
+select top 2 Nombre,Puntuacion
+from Cliente
+	inner join Puntuacion
+	on Puntuacion.Id = Cliente.id
+group by Nombre,Puntuacion
 
 --2.- Media de la puntuación de discos de los intérpretes que
 --comiencen con A y efectuada en sábado.
-
+select Interprete,AVG(Puntuacion) as media,DATENAME(DW,fecha) fecha
+from Puntuacion
+	inner join Disco
+	on Disco.IdDisco = Puntuacion.iddisco
+	inner join Interprete
+	on Interprete.IdInterprete = Disco.IdInterprete
+	where Interprete like 'A%' and DATENAME(DW,Fecha)='sábado'
+group by Interprete,fecha
 
 --3.- Clientes (dando su nombre) nacidos antes de 1975 que hayan
 --puntuado a los tipos que contengan 'rock'.
-
+select Nombre
+from Cliente
+	inner join Puntuacion
+	on Cliente.id=Puntuacion.Idcliente
+	inner join Disco
+	on Disco.IdDisco=Puntuacion.iddisco
+	inner join DiscoTipo
+	on disco.IdDisco=DiscoTipo.IdDisco
+	inner join Tipo
+	on DiscoTipo.IdTipo=tipo.IdTipo
+	where tipo like'%rock%'and FechaNacimiento<'1975'
 
 --4.- Disco (dando su título) con mayor media de puntuacion que haya sido
 --votado dos o más veces.
-
-
+select top 1 Titulo,AVG(Puntuacion),COUNT(*)
+from Disco
+	inner join Puntuacion
+	on Puntuacion.iddisco=Disco.IdDisco
+group by Titulo
+having COUNT(*)>='2'
+order by avg(Puntuacion) desc
 --5.- Intérprete que más veces haya sido puntuado.
-
+select top 1 Interprete, count(*) as puntuacion
+from Interprete
+	inner join Disco
+	on Interprete.IdInterprete = Disco.IdInterprete
+	inner join Puntuacion
+	on Disco.IdDisco = Puntuacion.iddisco
+group by Interprete	
+order by puntuacion desc
 
 --6.- Dos intérpretes con más discos.
-
+select top 2 with ties Interprete, count (Titulo) as discos
+from Interprete
+	inner join Disco
+	on Interprete.IdInterprete = Disco.IdInterprete
+group by Interprete
+order by discos desc
 
 --7 títulos de los discos que hayan recibido
 --alguna puntuación y el nombre del intérprete.
