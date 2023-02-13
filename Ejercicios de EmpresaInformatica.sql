6499ea5244449b6632b060158070effd8ec147a4
Alejandro <alejandroasp71@gmail.com>
2023-02-13 12:15:54 +0000

    arreglando ejercicios

--- Ejercicios de EmpresaInformatica.sql
+++ Ejercicios de EmpresaInformatica.sql
@@ -9,26 +9,29 @@ from Componente
 	where Tipo like '%ALMACENAMIENTO%' and Tipo not like '%VARIOS%'
 group by Tipo
 --2.- Tienda con mayores ventas en importe
-select top 1 NombreTienda, COUNT(*) as NumMayorVentas
+select top 1 NombreTienda, SUM(Cantidad*PrecioAplicado) as NumMayorVentas
 from Tienda
 	inner join Factura
 	on tienda.IdTienda = Factura.idTienda
 	inner join FacturaComponente
 	on Factura.NFactura = FacturaComponente.NFactura
 group by NombreTienda
-
+order by NumMayorVentas desc
 --3.- Tienda con mayor número de facturas realizadas (sacar todas las que coincidan)
-select top 1 with ties NombreTienda,count(Cantidad)
+select top 1 with ties NombreTienda,count(*) as facturas
 from Tienda
 	inner join Factura
 	on tienda.IdTienda = Factura.idTienda
-	inner join FacturaComponente
-	on Factura.NFactura = FacturaComponente.NFactura
 group by NombreTienda
-order by NombreTienda
+order by facturas desc
 --4.- Artículos vendidos, dando su nombre, indicando el nº de veces que referencia esté a NULL
 
+
+
 --5.- Importe de las facturas de las tiendas de Localidad 'La Laguna'
+
+
+
 
 --6.- Nº de componentes vendidos por tipo de los tipos que contengan 'impresora'
 
